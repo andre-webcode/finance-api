c8a65eaa6f1ff009dc74928bd0bd507222a39354
Luiz André <luizandre.progr@gmail.com>
2026-09-07 20:02:21 -0300

    feat: ajusta porta do servidor para deploy

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,8 @@ server.use(helmet())
 
 server.use(mainRouter);
 
+const PORT = process.env.PORT || 3001;
 
-server.listen(3001, () => {
-    console.log('Servidor rodando em 3001')
-});
+server.listen(PORT, () => {
+    console.log(`Servidor rodando em ${PORT}`);
+});;
